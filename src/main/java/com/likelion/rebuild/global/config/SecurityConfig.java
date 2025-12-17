bc966dd226f7a29fefa176aee0fb46b9edd59066
leeyumin626 <leeym7404@naver.com>
2025-12-17 14:24:09 +0900

    ccicd

--- src/main/java/com/likelion/rebuild/global/config/SecurityConfig.java
+++ src/main/java/com/likelion/rebuild/global/config/SecurityConfig.java
@@ -48,7 +48,8 @@ public class SecurityConfig {
                                 "/api/users/logout",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html",
+                                "/hello"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
